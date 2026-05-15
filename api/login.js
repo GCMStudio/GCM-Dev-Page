350f580401668825d253de74c478b9831a44afe0
GCMStudio <gcm_studio@outlook.com>
2026-05-14 23:45:47 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,24 @@
+// api/login.js
 
+export default function handler(req, res) {
+
+    if (req.method !== "POST") {
+        return res.status(405).json({
+            message: "Método não permitido"
+        });
+    }
+
+    const { username, password } = req.body;
+
+    if (username === "admin" && password === "21012014Gu*") {
+        return res.status(200).json({
+            status: "valido",
+            message: "Login válido"
+        });
+    }
+
+    return res.status(401).json({
+        status: "invalido",
+        message: "Login inválido"
+    });
+}

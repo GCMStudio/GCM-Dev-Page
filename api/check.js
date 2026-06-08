2427b64d63349641e4206baec019030a3f63ee51
GCMStudio <gcm_studio@outlook.com>
2026-06-08 17:29:51 -0300

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,21 +1,18 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
     try {
         const currentVersion = req.body?.version || "0.0.0";
 
-        const config = JSON.parse(
-            fs.readFileSync(
-                path.join(__dirname, "versions.json"),
-                "utf8"
-            )
-        );
+        const filePath = path.join(process.cwd(), "data", "versions.json");
+
+        const config = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
         const latest = config.latest;
         const latestInfo = config.versions[latest];
 
-        res.status(200).json({
+        return res.status(200).json({
             currentVersion,
             latestVersion: latest,
             needsUpdate: currentVersion !== latest,
@@ -26,8 +23,8 @@ module.exports = (req, res) => {
             changelog: latestInfo.changelog
         });
     } catch (err) {
-        res.status(500).json({
+        return res.status(500).json({
             error: err.message
         });
     }
-};
+}
